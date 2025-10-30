040132f80c7d0f97cc21ad44763367ec3d6b03d0
DarkoGjichev <darkogicev111@gmail.com>
2025-10-30 16:54:33 +0000

    add clients POST endpoint

--- server/server.js
+++ server/server.js
@@ -5,3 +5,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.post(`/api/clients`, (req, res) => {
+  const client = req.body;
+  res.json({ status: "success", received: client });
+});
+
+module.exports = app;
