ecb0dce58d110a6fadf57cc07164525e61fa21f0
DarkoGjichev <darkogicev111@gmail.com>
2025-10-30 10:10:14 +0000

    add id for html list and logic for setting the list empty on button click

--- extension/side-panel/sidePanel.js
+++ extension/side-panel/sidePanel.js
@@ -2,9 +2,11 @@ import { fetchUsers } from "./api/fetchUsers.js";
 
 const fetchButton = document.getElementById("fetchUsersButton");
 const dataStatus = document.getElementById("dataStatus");
+const dataList = document.getElementById("dataList");
 
 fetchButton.addEventListener("click", async () => {
   dataStatus.textContent = "Loading...";
+  dataList.innerHTML = "";
 
   try {
     const users = await fetchUsers();
